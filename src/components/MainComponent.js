43f00f7e9248164e1a354a86787c4fac4ca3adc5
Hong Ngoc Khanh <hong.nkhanh@gmail.com>
2021-03-04 15:05:28 +0700

    update transition group

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -80,7 +80,7 @@ const Main = (props) => {
     <div>
       <Header />
       <TransitionGroup>
-        <CSSTransition key={props.location.key} classNames="page" timeout={500}>
+        <CSSTransition key={props.location.pathname} classNames="page" timeout={500}>
           <Switch location={props.location}>
             <Route path='/home' component={HomePage} />
             <Route path='/about-us' component={() => 
